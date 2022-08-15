92ab37aa027e239fbf1de7f6c55d3eec6ea4f1c6
Sabah Rahal <sabahrahal1998@gmail.com>
2022-08-15 02:47:21 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let excuses = document.getElementById("excuses");
+
+  const random = array => {
+    return Math.floor(Math.random() * array.length);
+  };
+
+  const concatArray = () => {
+    let whoPhrase = who[random(who)];
+    let whatPhrase = what[random(what)];
+    let whenPhrase = when[random(what)];
+    return whoPhrase
+      .concat(" ")
+      .concat(whatPhrase)
+      .concat(" ")
+      .concat(whenPhrase);
+  };
+
+  excuses.innerHTML = concatArray();
 };
